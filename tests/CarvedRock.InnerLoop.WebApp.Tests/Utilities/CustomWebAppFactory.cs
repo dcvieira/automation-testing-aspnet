5555251a353a60f098685586cfff584f4579d1a7
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2024-06-25 18:38:53 -0300

    Add Test utilities

---
+++ tests/CarvedRock.InnerLoop.WebApp.Tests/Utilities/CustomWebAppFactory.cs
@@ -0,0 +1,41 @@
+﻿using CarvedRock.WebApp;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using NSubstitute;
+
+namespace CarvedRock.InnerLoop.WebApp.Tests.Utilities;
+public class CustomWebAppFactory(SharedFixture fixture) : WebApplicationFactory<Program>
+{
+    public SharedFixture SharedFixture => fixture;
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("test");
+
+        builder.ConfigureServices(ProvideSubstituteForProductService);
+
+
+        builder.ConfigureTestServices(services => services
+              .AddAuthentication(TestAuthHandler.SchemeName)
+              .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(TestAuthHandler.SchemeName, _ => { }));
+
+    }
+
+    private void ProvideSubstituteForProductService(IServiceCollection services)
+    {
+        var productService = services.SingleOrDefault(d => d.ServiceType == typeof(IProductService));
+        services.Remove(productService!);
+
+        var productServiceMock = Substitute.For<IProductService>();
+
+        productServiceMock.GetProductsAsync(Arg.Any<string>()).Returns(args => SharedFixture.OriginalProducts
+            .Where(p => p.Category == (string) args[0]).ToList());
+
+        productServiceMock
+            .GetProductsAsync()
+            .Returns(args => SharedFixture.OriginalProducts);
+
+        services.AddScoped(_ => productServiceMock);
+    }
+}
---
+++ tests/CarvedRock.InnerLoop.WebApp.Tests/Utilities/SharedFixture.cs
@@ -0,0 +1,38 @@
+﻿using Bogus;
+using CarvedRock.Core;
+
+namespace CarvedRock.InnerLoop.WebApp.Tests.Utilities;
+
+
+[CollectionDefinition(nameof(InnerLoopCollection))]
+public class InnerLoopCollection: ICollectionFixture<SharedFixture>
+{
+}
+public class SharedFixture : IAsyncLifetime
+{
+    public readonly Faker Faker = new();
+    public List<ProductModel> OriginalProducts { get; private set; } = new();
+    private static readonly List<string> _categories = ["boots", "equip", "kayak"];
+
+    public readonly Faker<ProductModel> ProductFaker = new Faker<ProductModel>()
+       .RuleFor(p => p.Id, f => f.UniqueIndex + 1)
+       .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+       .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
+       .RuleFor(p => p.Price, f => f.Random.Double(10, 1000))
+       .RuleFor(p => p.Category, f => f.PickRandom(_categories))
+       .RuleFor(p => p.ImgUrl, f => f.Image.PicsumUrl());
+
+    public Task InitializeAsync()
+    {
+        // Initialize the shared fixture
+        OriginalProducts = ProductFaker.Generate(10);
+
+        return Task.CompletedTask;
+    }
+
+    public  Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
+
